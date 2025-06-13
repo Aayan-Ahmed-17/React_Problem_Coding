80ff8dc530b06ce5ac8d92210c793bb31edc1efb
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-06-13 19:24:16 +0500

    checking useReducer return | returning state n dispatch only else undefine

--- src/reducer-practice/Counter/Counter.jsx
+++ src/reducer-practice/Counter/Counter.jsx
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
+
+const reducerFunction = (state, action) => {};
 
 const Counter = () => {
+  const [state, dispatch, a, b] = useReducer();
   const [count, setCount] = useState(0);
 
+  console.log("state====>", state);
+  console.log("dispatch===>", dispatch);
+  console.log("a===>", a);
+  console.log("b===>", b);
   return (
     <>
       <h1>Counter useReducer</h1>
