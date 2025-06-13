6f6b9edc312097b6b285aeebf09ab4e2d3eb4323
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-06-13 19:25:43 +0500

    removed unnecessary code | going to start useReducer

--- src/reducer-practice/Counter/Counter.jsx
+++ src/reducer-practice/Counter/Counter.jsx
@@ -1,21 +1,15 @@
-import React, { useReducer, useState } from "react";
+import React, { useReducer } from "react";
 
 const reducerFunction = (state, action) => {};
 
 const Counter = () => {
-  const [state, dispatch, a, b] = useReducer();
-  const [count, setCount] = useState(0);
-
-  console.log("state====>", state);
-  console.log("dispatch===>", dispatch);
-  console.log("a===>", a);
-  console.log("b===>", b);
+  const [state, dispatch] = useReducer();
   return (
     <>
       <h1>Counter useReducer</h1>
-      <h2>Counter : {count}</h2>
-      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
-      <button onClick={() => setCount((prev) => prev - 1)}>Decrement</button>
+      <h2>Counter : {state}</h2>
+      <button>Increment</button>
+      <button>Decrement</button>
     </>
   );
 };
