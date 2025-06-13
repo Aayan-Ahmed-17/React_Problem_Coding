699ad0acfb12a857d2c28513e97478e615e44754
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-06-13 19:30:51 +0500

    almost done | need to split code | change init state into obj

--- src/reducer-practice/Counter/Counter.jsx
+++ src/reducer-practice/Counter/Counter.jsx
@@ -1,15 +1,25 @@
 import React, { useReducer } from "react";
 
-const reducerFunction = (state, action) => {};
+const reducerFunction = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return (state = 0);
+  }
+};
 
 const Counter = () => {
-  const [state, dispatch] = useReducer();
+  const [state, dispatch] = useReducer(reducerFunction, 0);
   return (
     <>
       <h1>Counter useReducer</h1>
       <h2>Counter : {state}</h2>
-      <button>Increment</button>
-      <button>Decrement</button>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "RESET" })}>Reset </button>
     </>
   );
 };
