dbcfdefb5f107c211aa29a6bec5d42e0ab5277ea
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-06-15 19:23:38 +0500

    going to start form eg by own

--- src/reducer-practice/ReducerComponents.jsx
+++ src/reducer-practice/ReducerComponents.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Counter from "./Counter/Counter";
 import ToggleVisibility from "./Toggle-Visibility/ToggleVisibility";
+import Form from "./Form/Form";
 
 const ReducerComponents = () => {
   return (
     <>
       <h1>Reducer Component</h1>
       {/* <Counter /> */}
-      <ToggleVisibility/>
+      {/* <ToggleVisibility/> */}
+      <Form/>
     </>
   );
 };
